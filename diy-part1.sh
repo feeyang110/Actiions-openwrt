9e6b45099a3fa7fcdf9e92f70106585fbf378c5a
feeyang110 <40483313+feeyang110@users.noreply.github.com>
2024-04-13 23:08:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 # sed -i '3i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
-# sed -i '4i src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
+ sed -i '4i src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
